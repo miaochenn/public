b4a0522d4aaea2b8b1d43517b2c409326d34f5ac
苗景超 <miaojingchao@MacOS.local>
2019-08-01 22:16:13 +0800

    add redis zRevRange() demo

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 $redisObj = new Redis();
 $redisObj->connect('127.0.0.1', 6379);
+goto result;
 echo json_encode(get_class_methods($redisObj));
+
+// $redis->zIncrBy('zset_key', 1, 'member_one');
+// $redis->zIncrBy('zset_key', 1, 'member_two');
+// $redis->zIncrBy('zset_key', 1, 'member_three');
+
+result:
+$res = $redisObj->zRevRange('zset_key', 0, 5, true);
+var_dump($res);
